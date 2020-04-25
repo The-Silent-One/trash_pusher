8f26a3d4f4486508ee43ef998497fd943e23b67d
The-Silent-One <safou.mor@live.fr>
2020-04-25 22:25:39 +0100

    first

---
+++ sender.py
@@ -0,0 +1,32 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+port = 465
+sender = os.getenv("SENDER")
+password = os.getenv("PASSWORD")
+receiver = os.getenv("RECEIVER")
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    msg = MIMEMultipart("alternative")
+    msg["subject"] = "[Test] Automatic reminder"
+    msg["From"] = sender
+    msg["To"] = receiver
+    html = """
+    <html><body><p>
+    This is a friendly reminder to log in linux so that it can automaticaly push to git
+    <br><br>
+    Pushing for letter
+    </p></body></html>"""
+    
+    part = MIMEText(html, "html")
+    
+    msg.attach(part)
+    
+    server.login(sender, password)
+    server.sendmail(sender,receiver,msg.as_string())
+    
