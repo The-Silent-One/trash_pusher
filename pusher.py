24f51ca512313975bda608a95d72ad86fcd97da9
The-Silent-One <safou.mor@live.fr>
2020-04-30 18:37:05 +0100

    full automatic git commit

--- pusher.py
+++ pusher.py
@@ -1,9 +1,12 @@
-import os
-import time
+from os import system
+from time import ctime
 
 def send():
-    txt = "pushing on " + str(time.ctime()) + "\n"
+    txt = "pushing on " + str(ctime()) + "\n"
     with open("history.txt","a") as f:
         f.write(txt)
-    os.system("git add history.txt")
+    system("git add history.txt")
+    system('git commit -m "automatic'+ctime()+'"')
+    system("git push")
+
 send()
